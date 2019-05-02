f063af88bc1f57d5c0a2915b2cda2539be3c7406
Jay Walstab <jaywalstab@outlook.com>
2019-05-02 17:18:45 +0800

    first commit

---
+++ delta.js
@@ -0,0 +1,70 @@
+var express = require('express');
+var path= require('path');
+const bodyParser = require('body-parser');
+var app = express();
+var expressLayouts = require('express-ejs-layouts');
+
+app.use(bodyParser.json());
+
+var MongoClient = require('mongodb').MongoClient;
+var db;
+
+MongoClient.connect("mongodb+srv://quantum:Quantumdata123@cluster0-jjukt.mongodb.net/test?retryWrites=true", {useNewUrlParser: true}, function(err, database) {
+  if(err)
+  throw err;
+  db = database.db('iot');
+  app.listen(3000);
+  console.log("Delta is up and running on port 3000 since " + new Date().toLocaleDateString() + "  " + new Date().toLocaleTimeString());
+});
+
+
+////////WEB SERVER
+
+
+app.use(express.static(path.join(__dirname,'public')));
+app.use(expressLayouts);
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+app.get('/public/assets/images/favico.ico' , function(req , res){/*code*/});
+
+app.get("/", function(req, res) {
+  //res.sendFile(__dirname + '/Index.html');
+  res.render('index');
+});
+
+app.get("/:url", function(req, res) {
+  //res.sendFile(__dirname + '/' + req.params.url + '.html');
+  res.render(req.params.url);
+});
+
+
+//////////API 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
+app.post("/:deviceid", function(req, res) {
+  db.collection(req.params.deviceid).insertOne(req.body).then (function() {
+    res.send("Recieved: " + req.body);
+    res.end();
+  });
+});
+
+app.get("/:deviceid", function(req, res) {
+  db.collection(req.params.deviceid).find({}).toArray(function(err, docs){
+    res.send(docs);
+    res.end();
+  });
+});
+
+app.get("/:deviceid/last/:amount", function(req, res) {
+  var limitAmount = parseInt(req.params.amount);
+  db.collection(req.params.deviceid).find({}).sort( { _id : -1 } ).limit(limitAmount).toArray(function(err, docs){
+    res.send(docs);
+    res.end();
+  });
+});
