8564f71c77c39a46b92910e6f4f4e4870955fd51
Jay Walstab <jaywalstab@outlook.com>
2019-05-06 10:19:45 +0800

    v6

--- delta.js
+++ delta.js
@@ -6,10 +6,13 @@ var expressLayouts = require('express-ejs-layouts');
 
 app.use(bodyParser.json());
 
+
+//MongoClient.connect("mongodb+srv://quantum:Quantumdata123@cluster0-jjukt.mongodb.net/test?retryWrites=true", {useNewUrlParser: true}, function(err, database) {
 var MongoClient = require('mongodb').MongoClient;
 var db;
 
-MongoClient.connect("mongodb+srv://quantum:Quantumdata123@cluster0-jjukt.mongodb.net/test?retryWrites=true", {useNewUrlParser: true}, function(err, database) {
+  MongoClient.connect("mongodb://165.22.241.11:27017", {useNewUrlParser: true}, function(err, database) {
+  //MongoClient.connect("mongodb://127.0.0.1:27017", {useNewUrlParser: true}, function(err, database) {
   if(err)
   throw err;
   db = database.db('iot');
@@ -69,6 +72,15 @@ app.get("/:deviceid/last/:amount", function(req, res) {
   });
 });
 
+app.post("/:deviceid/last1/:amount", function(req, res) {
+  console.log(req.body);
+  var limitAmount = parseInt(req.params.amount);
+  db.collection(req.params.deviceid).find({}).sort( { _id : -1 } ).limit(limitAmount).toArray(function(err, docs){
+    res.send(docs);
+    res.end();
+  });
+});
+
 app.get("/:deviceid/date", function(req, res) {
   db.collection(req.params.deviceid).find({}).toArray(function(err, docs){
     arrayResult = [];
@@ -85,6 +97,29 @@ app.get("/:deviceid/date", function(req, res) {
       console.log(element);
     });
     res.send("finished!");
+    res.end();
+  });
+});
+
+app.post("/:deviceid/betweendates", function(req, res) {
+  db.collection(req.params.deviceid).find({}).toArray(function(err, docs){
+    arrayResult = [];
+    console.log(req.body);
+    to = new Date(req.body.to);
+    console.log(to);
+    from = new Date(req.body.from);
+    console.log(from);
+    docs.forEach(element => {
+      var check = new Date(element.time);
+      if((check.getTime() <= to.getTime() && check.getTime() >= from.getTime()))
+      {
+        arrayResult.push(element);
+      }
+    });
+    arrayResult.forEach(element => {
+      console.log(element);
+    });
+    res.send(arrayResult);
     res.end();
   });
 });
