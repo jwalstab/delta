89f5c94a57a97e28f7a1ef9f622b9d9536e29dfc
Jay Walstab <jaywalstab@outlook.com>
2019-05-06 10:42:53 +0800

    v7

--- delta.js
+++ delta.js
@@ -46,6 +46,7 @@ app.get("/:url", function(req, res) {
 //////////API SERVER
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods: GET, POST, PATCH, PUT, DELETE, OPTIONS")
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
 });
